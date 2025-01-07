30e468be1b56496f420dcaf4f94cd5ca60de75b6
maratbabaev <you@example.com>
2025-01-07 17:52:11 +0300

    2 commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,24 @@ window.addEventListener("resize", setVh);
 let topSlidersMenuItem = document.querySelectorAll(".top_sliders_menu_item");
 let topSlider = document.querySelectorAll(".top_slider");
 let contentSlider = document.querySelectorAll(".content_slider_swiper");
+let burger = document.querySelector(".burger");
+let header = document.querySelector(".header");
+let main = document.querySelector(".main");
+
+burger.addEventListener("click", () => {
+	burger.classList.toggle("active");
+	header.classList.toggle("active");
+	main.classList.toggle("bg-active");
+	document.body.classList.toggle("hidden");
+});
+
+main.addEventListener("click", () => {
+	burger.classList.remove("active");
+	header.classList.remove("active");
+	main.classList.remove("bg-active");
+	document.body.classList.remove("hidden");
+});
+
 topSlidersMenuItem[0].classList.add("active");
 topSlider[0].classList.add("active");
 
